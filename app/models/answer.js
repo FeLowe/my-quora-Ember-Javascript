d078edd70d2bdb8e51d2fc66d4439e1cb336daac
Fernanda Lowe <fe_lowe@hotmail.com>
2016-06-10 16:42:17 -0700

    display questions and questions details stored in database

---
+++ app/models/answer.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+// import attr from 'ember-data/attr';
+// import { belongsTo, hasMany } from 'ember-data/relationships';
+
+export default DS.Model.extend({
+  userAnswer: DS.belongsTo('question', { async: true }),
+  respondent: DS.attr(),
+  date: DS.attr(),
+});
---
+++ app/routes/index.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+  return this.store.findAll('question');
+},
+  actions: {
+    destroyQuestionRouter(question) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
+});
